80423f92ddbf26ffac76e957c3d33f27ab6b84f2
Jerko J <83344666+JJ-Cro@users.noreply.github.com>
2023-11-10 14:03:52 +0100

    Added ts

---
+++ src/MTtoJSON.ts
@@ -0,0 +1,12 @@
+/* 1. Reports just like the reports generated from mql5 (monthly gain, chart of performance, drawdown, balance, equity, profit etc)
+2. Accumulated Today profit/loss
+3. Details of todays position opened and closed
+4. Time to report (everyday)
+5. Same as above only for weekly
+6. Same as above only for monthly
+7. Same as above only for All Time
+8. Same as above only for Custom Date
+9. Ability to input the start date (for example: my account is opened from 2010, but i only want to get results start from 5 October 2021 till now) */
+
+
+
